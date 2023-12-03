18faa6b63d0680cc7efa74f6dc27e47f8dc2a644
MehdiMarjani <113903979+Sevilex@users.noreply.github.com>
2023-12-03 05:05:38 -0800

    V1.0.3(Add App Module & Constants & QuestionApi & TriviaApplication)

---
+++ app/src/main/java/com/example/jettrivia/utli/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.jettrivia.utli
+
+object Constants {
+    //https://raw.githubusercontent.com/itmmckernan/triviaJSON/master/world.json
+const val BASE_URL = "https://raw.githubusercontent.com/itmmckernan/triviaJSON/master/world/"
+}
